bdf9e1172441bb292f3407dcc67ed5466b505787
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-09 13:00:10 -0600

    fix(health): make bare health file
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/which-colorscheme/health.lua
+++ lua/which-colorscheme/health.lua
@@ -1,15 +1,27 @@
+local Util = require('which-colorscheme.util')
+
 ---@class WhichColorscheme.Health
 local M = {}
 
 function M.check()
-  vim.health.start('WhichColorscheme')
+  vim.health.start('Setup')
 
-  if vim.g.WhichColorscheme_setup == 1 then
-    vim.health.ok('WhichColorscheme has been setup!')
+  if not Util.mod_exists('which-key') then
+    vim.health.error('`which-key.nvim` is not installed!')
     return
   end
+  vim.health.ok('`which-key.nvim` installed!')
 
-  vim.health.error('WhichColorscheme has not been setup correctly!')
+  if vim.g.WhichColorscheme_setup ~= 1 then
+    vim.health.error('`which-colorscheme.nvim` has not been setup correctly!')
+    return
+  end
+  vim.health.ok('`which-colorscheme.nvim` setup!')
+
+  vim.health.start('Config')
+  for k, v in pairs(require('which-colorscheme.config').config) do
+    vim.health.info(('`%s`: `%s`'):format(k, vim.inspect(v)))
+  end
 end
 
 return M
